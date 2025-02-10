806eb622ac42ac731857d5f2286521c587524752
Your Name <your-email@example.com>
2025-02-10 17:49:15 +0700

    upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-
+import { BookingsModule } from './bookings/bookings.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/coworking-space'),
